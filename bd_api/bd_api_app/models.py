98d0002cad5fa0a971fd3125e835806dcac7537e
melquetrindade <melquetrindade654@gmail.com>
2024-03-16 15:21:34 -0300

    add a opção ativo nas class pessoas e produtos

--- bd_api/bd_api_app/models.py
+++ bd_api/bd_api_app/models.py
@@ -5,7 +5,7 @@ class Pessoa(models.Model):
     nome = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     numTelefone = models.CharField(max_length=11, unique=True)
-    status = models.BooleanField(default=False)
+    status = models.BooleanField(default=True)
     dataNascimento = models.CharField(max_length=15)
 
     def __str__(self):
@@ -72,7 +72,7 @@ class Produto(models.Model):
     qtdMinima = models.IntegerField()
     quantidade = models.IntegerField()
     valor = models.FloatField()
-    status = models.BooleanField(default=False)
+    status = models.BooleanField(default=True)
 
     def __str__(self):
         return self.codigoBarras
